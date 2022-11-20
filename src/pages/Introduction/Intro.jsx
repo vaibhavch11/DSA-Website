e6b1f61c76274080da5f6de8b65b45313e3773da
vaibhav chauhan <chauhanvaibhav1105@gmail.com>
2022-11-20 17:04:06 +0530

    text editor issue

--- src/pages/Introduction/Intro.jsx
+++ src/pages/Introduction/Intro.jsx
@@ -1,52 +1,46 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import "../Introduction/Intro.scss";
-import TeamTopicPage from '../teamTopicPage/teamTopicPage';
+import TeamTopicPage from "../teamTopicPage/teamTopicPage";
 
-function Intro({data}) {
+function Intro({ data }) {
+  console.log(data, "Intro main hu");
 
-  console.log(data , "Intro main hu");
+  const [intro, setIntro] = useState([]);
 
-  
+  useEffect(() => {
+    setIntro((intro) => {
+      let arr = [];
+      let dataObjectArray = Object.entries(data);
+      let introObjectArray = Object.entries(dataObjectArray[1][1]);
 
-   const [intro, setIntro] = useState([]);
-
-   
-
-   useEffect(() => {
-     setIntro((intro)=>{
-       let arr = [];
-       let dataObjectArray= Object.entries(data);
-       let introObjectArray = Object.entries(dataObjectArray[1][1]);
-
-       console.log(introObjectArray , "data object in useEffect");
+      console.log(introObjectArray, "data object in useEffect");
 
       //  console.log(dataObjectArray , "data object in useEffect");
 
-       for(var i = 0;i<introObjectArray.length;++i){
-        arr.push(<div className="introData" >
-                 <h1 className='introHeading' id={`${introObjectArray[i][0]}`}>{introObjectArray[i][0]}</h1>
-                 <p className='introPara'>{introObjectArray[i][1]}</p>
-          </div>)
-       }
-       return [...arr];
-     })
+      for (var i = 0; i < introObjectArray.length; ++i) {
+        arr.push(
+          <div className="introData">
+            <h1 className="introHeading" id={`${introObjectArray[i][0]}`}>
+              {introObjectArray[i][0]}
+            </h1>
+            <p className="introPara">{introObjectArray[i][1]}</p>
+          </div>
+        );
+      }
+      return [...arr];
+    });
+  }, []);
 
-   
-   }, [])
-
-
-
-   
   return (
-    <div>
+    <div className="intro">
+      {intro}
 
-      
-  {intro}
-  <TeamTopicPage data={data}/>
+      <div className="editor">
+        <TeamTopicPage data={data} />
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Intro
+export default Intro;
--- src/pages/teamTopicPage/teamTopicPage.jsx
+++ src/pages/teamTopicPage/teamTopicPage.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
-import "../Introduction/Intro.scss"
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import draftToHtml from 'draftjs-to-html';
-import htmlToDraft from 'html-to-draftjs';
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw} from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
 
-function TeamTopicPage({data}){
-    console.log("Inside team topic page");
+export default class teamTopicPage extends Component {
+
+    state = {
+      editorState: EditorState.createEmpty(),
+    }
+
+    onEditorStateChange = (editorState) => {
+       this.setState({
+        editorState,
+       });
+    }
+
     
-    let dataObjectArray= Object.entries(data);
-    let string_html_data='';
-    
-    let subTopicArray=[];
+
+    dataObjectArray= Object.entries(this.props.data);
+    string_html_data='';
+
+    subTopicArray=[];
     for(var i=1;i<dataObjectArray.length;++i){
         subTopicArray.push(Object.entries(dataObjectArray[i][1]));
     }
@@ -21,43 +30,94 @@ function TeamTopicPage({data}){
     for(var i=0;i<subTopicArray.length;++i){
         string_html_data=string_html_data+subTopicArray[i][0]+subTopicArray[i][1];
     }
-    
-    //converting html string to draft
-    const contentBlock=htmlToDraft(string_html_data);
-    function changeToEdit(){
-        setEdit((edit)=>{
-            return true;
-        })
-    }
-    const [edit, setEdit] = useState(false)
-    let editorState
 
+
+
+    contentBlock=htmlToDraft(string_html_data);
     if (contentBlock) {
-        const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
-        editorState = EditorState.createWithContent(contentState);
-    }
+      const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+      editorState = EditorState.createWithContent(contentState);
+  }
     
 
-       
+
+  render() {
+    const {editorState} = this.state;
+    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+
     return (
-        <div>
-          <Editor
-            editorState={editorState}
-            wrapperClassName="demo-wrapper"
-            editorClassName="demo-editor"
-            onEditorStateChange={changeToEdit}
-            readOnly={edit}
-          />
-          <textarea
-            disabled
-            value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-          />
-          <button onClick={changeToEdit} type="button">Edit!</button>
-        </div>
-      );
-    }
+      <div>
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange}
+          // readOnly = {true}
+        />
+        
+        <textarea disabled value={draftToHtml(convertToRaw(editorState.getCurrentContent(editorState.getCurrentContent())))}></textarea>
+        <button  type="button">Edit!</button>
+      </div>
+    );
+  }
+}
 
+// import React from 'react'
+// import { useState,useEffect } from 'react';
+// import "../Introduction/Intro.scss"
+// import { EditorState, convertToRaw, ContentState } from 'draft-js';
+// import { Editor } from 'react-draft-wysiwyg';
+// import draftToHtml from 'draftjs-to-html';
+// import htmlToDraft from 'html-to-draftjs';
 
+// function TeamTopicPage({data}){
+//     console.log("Inside team topic page");
 
+//     let dataObjectArray= Object.entries(data);
+//     let string_html_data='';
 
-export default TeamTopicPage;
+//     let subTopicArray=[];
+//     for(var i=1;i<dataObjectArray.length;++i){
+//         subTopicArray.push(Object.entries(dataObjectArray[i][1]));
+//     }
+
+//     //converting json to html string
+//     for(var i=0;i<subTopicArray.length;++i){
+//         string_html_data=string_html_data+subTopicArray[i][0]+subTopicArray[i][1];
+//     }
+
+//     //converting html string to draft
+//     const contentBlock=htmlToDraft(string_html_data);
+//     function changeToEdit(){
+//         setEdit((edit)=>{
+//             return true;
+//         })
+//     }
+//     const [edit, setEdit] = useState(false)
+//     let editorState
+
+//     if (contentBlock) {
+//         const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+//         editorState = EditorState.createWithContent(contentState);
+//     }
+
+//     return (
+//         <div>
+//           <Editor
+//             editorState={editorState}
+//             wrapperClassName="demo-wrapper"
+//             editorClassName="demo-editor"
+//             onEditorStateChange={changeToEdit}
+//             readOnly={edit}
+//           />
+//           <textarea
+//             disabled
+//             value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+//           />
+//           <button onClick={changeToEdit} type="button">Edit!</button>
+//         </div>
+//       );
+//     }
+
+// export default TeamTopicPage;
