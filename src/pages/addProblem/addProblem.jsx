490cb0aa464adbdfd2d4d5636614a5aa5e643448
Kunal Siwal <kunalissiwal@gmail.com>
2022-12-11 11:17:47 +0530

    added add problem

--- src/pages/addProblem/addProblem.jsx
+++ src/pages/addProblem/addProblem.jsx
@@ -1,7 +1,11 @@
 import MyEditor from "../../components/TextEditor/textEditor";
+import { useState } from "react";
 
 const AddProblem=()=>{
+    const [text, setText] = useState("")
+    console.log("add problem data here ",text);
     return (<div>
-        <MyEditor />
+        <MyEditor textData={text} setTextContent={setText} />
     </div>)
 }
+export default AddProblem
